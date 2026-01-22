406d0dab629766b5e4885a4882c54aa0400d37c0
rgarci06 <rgarci@insdanielblanxart.cat>
2026-01-22 16:08:14 +0100

    GOD

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,18 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# Cuando Vercel te dé tu URL (ej: lux-restaurant.vercel.app), ponla aquí:
+origins = [
+    "http://localhost:5173",
+    "https://lux_restaurant.app" 
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
